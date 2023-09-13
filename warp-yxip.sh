3203d792583f59083bcb546395e37c5a0ad26b1f
cmliu.net <admin@cmliu.net>
2023-09-13 16:46:12 +0800

    Update and rename warp-yxip-mac.sh to warp-yxip.sh

--- warp-yxip.sh
+++ warp-yxip.sh
@@ -17,27 +17,23 @@ yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-for i in "${CMD[@]}"; do
-    SYS="$i" && [[ -n $SYS ]] && break
-done
-
-for ((int = 0; int < ${#REGEX[@]}; int++)); do
-    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
-        SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
-    fi
-done
-
+# 选择客户端 CPU 架构
 archAffix(){
     case "$(uname -m)" in
+        i386 | i686 ) echo '386' ;;
         x86_64 | amd64 ) echo 'amd64' ;;
         armv8 | arm64 | aarch64 ) echo 'arm64' ;;
+        s390x ) echo 's390x' ;;
         * ) red "不支持的CPU架构!" && exit 1 ;;
     esac
 }
 
-endpointyx(){
+endpointyx(){    
     # 下载优选工具软件，感谢某匿名网友的分享的优选工具
-    wget https://gitlab.com/Misaka-blog/warp-script/-/raw/main/files/warp-yxip/warp-darwin-$(archAffix) -O warp
+    wget https://gitlab.com/Misaka-blog/warp-script/-/raw/main/files/warp-yxip/warp-linux-$(archAffix) -O warp
+    
+    # 取消 Linux 自带的线程限制，以便生成优选 Endpoint IP
+    ulimit -n 102400
     
     # 启动 WARP Endpoint IP 优选工具
     chmod +x warp && ./warp >/dev/null 2>&1
@@ -69,6 +65,11 @@ endpoint4(){
             break
         fi
         temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
+        n=$(($n + 1))
+        if [ $n -ge $iplist ]; then
+            break
+        fi
+        temp[$n]=$(echo 162.159.204.$(($RANDOM % 256)))
         n=$(($n + 1))
         if [ $n -ge $iplist ]; then
             break
@@ -111,6 +112,12 @@ endpoint4(){
             break
         else
             temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
+            n=$(($n + 1))
+        fi
+        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+            break
+        else
+            temp[$n]=$(echo 162.159.204.$(($RANDOM % 256)))
             n=$(($n + 1))
         fi
         if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
@@ -187,7 +194,7 @@ endpoint6(){
 menu(){
     clear
     echo "#############################################################"
-    echo -e "#           ${RED}WARP Endpoint IP 一键优选脚本 for Mac${PLAIN}           #"
+    echo -e "#               ${RED}WARP Endpoint IP 一键优选脚本${PLAIN}               #"
     echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
     echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
     echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
