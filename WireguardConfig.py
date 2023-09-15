9be8052e233c35a209f94c9ecd441e45d72230d8
cmliu.net <admin@cmliu.net>
2023-09-15 23:56:29 +0800

    Update WireguardConfig.py

--- WireguardConfig.py
+++ WireguardConfig.py
@@ -1,4 +1,10 @@
 import csv
+import sys
+
+# 获取命令行参数
+ipv6 = sys.argv[1]
+private_key = sys.argv[2]
+public_key = sys.argv[3]
 
 def process_csv(file_path):
     with open(file_path, 'r') as file:
@@ -11,7 +17,7 @@ def process_csv(file_path):
                 ip_port = row[0].strip()
                 ip, port = ip_port.split(":")
                 warp_name = f"Warp{i:02d}"
-                new_line = f"- name: {warp_name}\n  type: wireguard\n  server: {ip}\n  port: {port}\n  ip: 172.16.0.2\n  ipv6: 2606:4700:110:8db8:9c99:ddd0:61b1:9eae\n  private-key: xxxx\n  public-key: xxxx\n  udp: true\n  mtu: 1280\n  remote-dns-resolve: true\n  dns: [ 1.1.1.1, 1.0.0.1 ]\n\n"
+                new_line = f"- name: {warp_name}\n  type: wireguard\n  server: {ip}\n  port: {port}\n  ip: 172.16.0.2\n  ipv6: {ipv6}\n  private-key: {private_key}\n  public-key: {public_key}\n  udp: true\n  mtu: 1280\n  remote-dns-resolve: true\n  dns: [ 1.1.1.1, 1.0.0.1 ]\n\n"
                 output_file.write(new_line)
 
     print("结果已保存到 clash_yaml.txt 文件")
