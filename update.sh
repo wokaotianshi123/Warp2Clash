3488d810f5f40b768e5a19ba8f8815906d0d8a13
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-04-10 20:02:36 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export LANG=zh_CN.UTF-8
+DOMAIN="cftokv.wofuck.rr.nu"
+TOKEN="papa"
+if [ -n "$1" ]; then 
+  FILENAME="$1"
+else
+  echo "无文件名"
+  exit 1
+fi
+BASE64_TEXT=$(head -n 65 $FILENAME | base64)
+curl -k "https://$DOMAIN/$FILENAME?token=$TOKEN&b64=$BASE64_TEXT"
+echo "更新数据完成"
